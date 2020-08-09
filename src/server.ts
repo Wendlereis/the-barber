6c3af51912fe90ffe0137d2b7f7335800796518a
Wendler Eis <wendler_eis@hotmail.com>
2020-08-08 23:15:04 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import 'reflect-metadata';
 import express from 'express';
 import 'express-async-errors';
 
+import cors from 'cors';
+
 import routes from './routes';
 
 import fileUploadConfig from './config/fileUpload';
@@ -14,6 +16,7 @@ const app = express();
 
 app.use(express.json());
 app.use('/files', express.static(fileUploadConfig.destination));
+app.use(cors());
 app.use(routes);
 app.use(exceptionMiddleware);
 
